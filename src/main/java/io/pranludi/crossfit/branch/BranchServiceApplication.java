515f99a41181d26a001f44a8b3eb6a23ad7eea27
daekwon <pranludi@gmail.com>
2025-04-16 13:32:02 +0900

    grpc metadata 에 jwt 적용

--- src/main/java/io/pranludi/crossfit/branch/BranchServiceApplication.java
+++ src/main/java/io/pranludi/crossfit/branch/BranchServiceApplication.java
@@ -1,13 +1,16 @@
 package io.pranludi.crossfit.branch;
 
+import io.pranludi.crossfit.branch.domain.server.ServerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ServerProperties.class)
 @SpringBootApplication
 public class BranchServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BranchServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BranchServiceApplication.class, args);
+    }
 
 }
