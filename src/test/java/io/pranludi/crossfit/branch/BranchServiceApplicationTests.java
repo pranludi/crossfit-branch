624aa728465130c03aeae8be6bde26226a4a6d33
daekwon <pranludi@gmail.com>
2025-04-17 02:10:48 +0900

    fix

--- src/test/java/io/pranludi/crossfit/branch/BranchServiceApplicationTests.java
+++ src/test/java/io/pranludi/crossfit/branch/BranchServiceApplicationTests.java
@@ -1,15 +1,11 @@
 package io.pranludi.crossfit.branch;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BranchServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.assertThat(10).isEqualTo(10);
     }
 
 }
